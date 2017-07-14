cfdf5a0fc1b5919cfb0592160a367870662a66eb
maty <maty@matydeMacBook-Pro.local>
2017-07-14 19:56:54 +0800

    pc

--- pc/img/baidu.js
+++ pc/img/baidu.js
@@ -6,6 +6,11 @@ var _hmt = _hmt || [];
 		//移除loading
 		var $loading = document.getElementById('app-loading');
 		$loading && document.body.removeChild($loading);
+		//百度统计
+		var hm = document.createElement("script");
+		hm.src = "//hm.baidu.com/hm.js?78f3092ce7922d42045bc1f81f5e82b4";
+		//var s = document.getElementsByTagName("script")[0];
+		//s.parentNode.insertBefore(hm, s);
 		document.body.appendChild(hm);
 	};
 	document.addEventListener("DOMContentLoaded", handler, false);
